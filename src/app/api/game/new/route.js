305f1b767d009c41a3476a7fcec6b3bd19fa3413
mshahzaib101 <mshahzaib101ed@gmail.com>
2024-10-22 11:29:38 +0500

    update joinGameSession API to support multiplayer functionality

--- src/app/api/game/new/route.js
+++ src/app/api/game/new/route.js
@@ -1,15 +1,17 @@
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase-server";
 import { v4 as uuidv4 } from "uuid"; // Import UUID generator
 
-export async function POST() {
+export async function POST(request) {
   try {
+    const { gameMode } = await request.json();
+
     // Generate a unique gameId
     const gameId = uuidv4();
-    console.log("gameId", gameId);
+
     // Fetch the words list from Firestore
     const docRef = db.collection("gameConfig").doc("v1");
     const docSnap = await docRef.get();
-    console.log("docSnap", docSnap);
+
     if (docSnap.exists) {
       const { wordsList, maxRounds } = docSnap.data();
 
@@ -19,13 +21,28 @@ export async function POST() {
 
       // Save the random word as the answer in the Firestore games collection with the generated gameId
       const gameDocRef = db.collection("games").doc(gameId);
-      await gameDocRef.set({
-        answer: randomWord,
-        maxRounds,
-        wordsList, // Store the words list in this game
-        currentRound: 0, // Initialize the current round to 0
-        gameStatus: "playing",
-      });
+
+      let gameObj;
+      if (gameMode === "single") {
+        gameObj = {
+          answer: randomWord,
+          maxRounds,
+          wordsList, // Store the words list in this game
+          currentRound: 0, // Initialize the current round to 0
+          gameStatus: "playing",
+          mode: "single",
+        };
+      } else {
+        gameObj = {
+          answer: randomWord,
+          maxRounds,
+          wordsList, // Store the words list in this game
+          gameStatus: "playing",
+          mode: "multi",
+        };
+      }
+
+      await gameDocRef.set(gameObj);
 
       return new Response(JSON.stringify({ gameId, maxRounds }), {
         status: 200,
@@ -36,7 +53,7 @@ export async function POST() {
       });
     }
   } catch (error) {
-    console.error("Error starting new game:", error);
+    console.log("error", error);
     return new Response(JSON.stringify({ error: "Failed to start game" }), {
       status: 500,
     });
